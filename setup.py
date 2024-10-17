247efb1699602c24eb1dad525cbe772dad5a2c84
Ogun Akar <71190825+ogunakar9@users.noreply.github.com>
2024-10-17 14:39:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in nilighttheme/__init__.py
@@ -10,17 +8,18 @@ with open('nilighttheme/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+# Reading requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
-	name='nilighttheme',
-	version=version,
-	description='Light Material Theme for Frappe',
-	author='Randy Lowery',
-	author_email='lowerymayorga@gmail.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+    name='nilighttheme',
+    version=version,
+    description='Light Material Theme for Frappe',
+    author='Randy Lowery',
+    author_email='lowerymayorga@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=requirements,
 )
